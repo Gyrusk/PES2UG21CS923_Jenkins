ffa2be80bf35d13e042d8beab7398bf9d9e8b856
unknown <sathviksrikanth14@gmail.com>
2024-03-08 12:23:56 +0530

    Have updated file

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run and have also changes\n";
+  cout << "I have successfully built and run and have also changes will commit now\n";
   return 0;
 }
